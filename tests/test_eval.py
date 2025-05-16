6618bea44d4b4a584e62edcbb830895fb98d93bb
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-16 11:24:03 -0700

    tests: added testing for alternative metrics

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -435,3 +435,33 @@ def test_eval_downsampled_cells():
         assert os.path.exists(f"{OUTDIR}/celltype_{x}_real_de_results_control.csv"), (
             f"Expected file for real DE results missing for celltype: {x}"
         )
+
+
+def test_eval_alt_metric():
+    adata_real = build_random_anndata()
+    adata_pred = downsample_cells(adata_real, fraction=0.5)
+    evaluator = MetricsEvaluator(
+        adata_pred=adata_pred,
+        adata_real=adata_real,
+        include_dist_metrics=True,
+        control_pert=CONTROL_VAR,
+        pert_col=PERT_COL,
+        celltype_col=CELLTYPE_COL,
+        output_space="gene",
+        shared_perts=None,
+        outdir=OUTDIR,
+        class_score=True,
+        metric="anderson",
+    )
+    evaluator.compute()
+
+    for x in np.arange(N_CELLTYPES):
+        assert os.path.exists(f"{OUTDIR}/celltype_{x}_downstream_de_results.csv"), (
+            f"Expected file for downstream DE results missing for celltype: {x}"
+        )
+        assert os.path.exists(f"{OUTDIR}/celltype_{x}_pred_de_results_control.csv"), (
+            f"Expected file for predicted DE results missing for celltype: {x}"
+        )
+        assert os.path.exists(f"{OUTDIR}/celltype_{x}_real_de_results_control.csv"), (
+            f"Expected file for real DE results missing for celltype: {x}"
+        )
