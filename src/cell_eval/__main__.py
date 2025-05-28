ebeb2e2ffc953454b59ee457154110fcaa233e6d
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-28 16:48:45 -0700

    feat: implementt current behavior into cell-eval run subcommand

--- src/cell_eval/__main__.py
+++ src/cell_eval/__main__.py
@@ -1,166 +1,27 @@
-import argparse
+import argparse as ap
 
-import yaml
+from .cli import parse_args_run, run_evaluation
 
 
-def parse_args():
-    """
-    CLI for evaluation
-    """
-    parser = argparse.ArgumentParser(description="Evaluate a trained cell state model.")
-    parser.add_argument(
-        "-p",
-        "--adata-pred",
-        type=str,
-        help="Path to the predicted adata object to evaluate",
-        required=True,
-    )
-    parser.add_argument(
-        "-r",
-        "--adata-real",
-        type=str,
-        help="Path to the real adata object to evaluate against",
-        required=True,
-    )
-    parser.add_argument(
-        "--control-pert",
-        type=str,
-        default="non-targeting",
-        help="Name of the control perturbation",
-    )
-    parser.add_argument(
-        "--pert-col",
-        type=str,
-        default="target_name",
-        help="Name of the column designated perturbations",
-    )
-    parser.add_argument(
-        "--celltype-col",
-        type=str,
-        help="Name of the column designated celltype (optional)",
-    )
-    parser.add_argument(
-        "--output-space",
-        type=str,
-        default="gene",
-    )
-    parser.add_argument(
-        "-o",
-        "--outdir",
-        type=str,
-        default="./cell-eval-outdir",
-        help="Output directory to write to",
-    )
-    parser.add_argument(
-        "--skip-dist-metrics",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--skip-de-metrics",
-        action="store_false",
-    )
-    parser.add_argument(
-        "--skip-class-score",
-        action="store_false",
-    )
-    parser.add_argument(
-        "--num-threads",
-        type=int,
-    )
-    parser.add_argument(
-        "--batch-size",
-        type=int,
-    )
-    parser.add_argument(
-        "--skip-normlog-check",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--minimal-eval",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--metric",
-        type=str,
-        default="wilcoxon",
-    )
-    parser.add_argument(
-        "--fdr-threshold",
-        type=float,
-        default=0.05,
-    )
-    parser.add_argument(
-        "--config",
-        type=str,
-        help="Sets all options via yaml - ignores CLI if used",
-    )
+def get_args():
+    parser = ap.ArgumentParser()
+    subparsers = parser.add_subparsers(required=True, dest="subcommand")
+
+    parser_prep = subparsers.add_parser("prep")
+    parse_args_run(subparsers.add_parser("run"))
+
     return parser.parse_args()
-
-
-def run_evaluation(args: argparse.ArgumentParser):
-    from .metric_evaluator import MetricsEvaluator
-
-    print("Reading adata objects")
-    if args.config:
-        # Read in config file
-        with open(args.config, "r") as f:
-            config = yaml.safe_load(f)
-
-        # Create the evaluator
-        evaluator = MetricsEvaluator(
-            path_pred=args.adata_pred,
-            path_real=args.adata_real,
-            include_dist_metrics=config["include_dist_metrics"],
-            control_pert=config["control_pert"],
-            pert_col=config["pert_col"],
-            celltype_col=config["celltype_col"],
-            output_space=config["output_space"],
-            shared_perts=config["shared_perts"],
-            outdir=config["outdir"],
-            de_metric=config["de_metric"],
-            class_score=config["class_score"],
-            n_threads=config["n_threads"] if "n_threads" in config else None,
-            batch_size=config["batch_size"] if "batch_size" in config else None,
-            metric=config["metric"] if "metric" in config else "wilcoxon",
-            skip_normlog_check=config.get("skip_normlog_check", False),
-        )
-    else:
-        evaluator = MetricsEvaluator(
-            path_pred=args.adata_pred,
-            path_real=args.adata_real,
-            include_dist_metrics=args.skip_dist_metrics,
-            control_pert=args.control_pert,
-            pert_col=args.pert_col,
-            celltype_col=args.celltype_col,
-            output_space=args.output_space,
-            outdir=args.outdir,
-            de_metric=args.skip_de_metrics,
-            class_score=args.skip_class_score,
-            n_threads=args.num_threads,
-            batch_size=args.batch_size,
-            skip_normlog_check=args.skip_normlog_check,
-            minimal_eval=args.minimal_eval,
-            metric=args.metric,
-            fdr_threshold=args.fdr_threshold,
-        )
-
-    print("Running evaluation")
-    # Compute the metrics
-    evaluator.compute()
-
-    # Save the metrics
-    evaluator.save_metrics_per_celltype()
-    print("Done")
 
 
 def main():
     """
     Main function to run the evaluation.
     """
-
-    # Parse arguments
-    args = parse_args()
-    run_evaluation(args)
+    args = get_args()
+    if args.subcommand == "run":
+        run_evaluation(args)
+    elif args.subcommand == "pred":
+        print("unimplemented")
 
 
 if __name__ == "__main__":
---
+++ src/cell_eval/cli/__init__.py
@@ -0,0 +1,3 @@
+from ._run import parse_args_run, run_evaluation
+
+__all__ = [parse_args_run, run_evaluation]
