7f55304bfb65d504121f849414b853a5a7537403
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-06-18 11:28:48 -0700

    feat: added check on lognorm status

--- src/cell_eval/utils.py
+++ src/cell_eval/utils.py
@@ -1,4 +1,34 @@
 import anndata as ad
+import numpy as np
+
+EPSILON = 1e-6
+
+
+def guess_is_lognorm(
+    adata: ad.AnnData,
+    n_cells: int | float = 5e2,
+) -> bool:
+    """Guess if the input is integer counts or log-normalized.
+
+    This is an _educated guess_ based on whether the fractional component of cell sums.
+    This _will not be able_ to distinguish between normalized input and log-normalized input.
+
+    Returns:
+        bool: True if the input is lognorm, False otherwise
+    """
+    # Determine the number of cells to use for the guess
+    n_cells = int(min(adata.shape[0], n_cells))
+
+    # Pick a random subset of cells
+    cell_mask = np.random.choice(adata.shape[0], n_cells, replace=False)
+
+    # Sum the counts for each cell
+    cell_sums = adata.X[cell_mask].sum(axis=1)  # type: ignore (can be float but super unlikely)
+
+    fracs, _ = np.modf(cell_sums)
+
+    # Check if all fractional components are less than EPSILON (i.e. zero)
+    return bool(np.any(np.abs(fracs) > EPSILON))
 
 
 def split_anndata_on_celltype(
